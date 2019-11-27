d81700acf1fcf036169a9f2acbf35c5d0001cdce
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-11-27 03:15:48 +0000

    Created FruitGems.js template.
    Just a mess around bit of code.

---
+++ FruitGemz.js
@@ -0,0 +1,73 @@
+// ::>> Notes:
+// ::>> Notes:  Make gemz do something odd.
+// ::>> Notes:
+
+
+(function () {
+  if(document.body.section.classList === 'gemz'){
+    this?
+    target?
+    object?
+    
+  }
+  if(document.body.section.classList === 'fruit-gemz'){
+  
+  }
+  if(document.body.section.classList === 'apple-gemz'){
+  
+  }
+  if(document.body.section.classList === 'orange-gemz'){
+  
+  }  
+  if(document.body.section.classList === 'kiwi-gemz'){
+  
+  }  
+  if(document.body.section.classList === 'mango-gemz'){
+  
+  }  
+  if(document.body.section.classList === 'soursop-gemz'){
+  
+  }  
+  if(document.body.section.classList === 'pineapple-gemz'){
+  
+  } 
+  if(document.body.section.classList === 'guava-gemz'){
+  
+  }
+  if(document.body.section.classList === 'grape-gemz'){
+  
+  }  
+  if(document.body.section.classList === 'bannana-gemz'){
+  
+  }
+  if(document.body.section.classList === 'date-gemz'){
+  
+  }
+  if(document.body.section.classList === 'lime-gemz'){
+  
+  }
+  if(document.body.section.classList === 'lemon-gemz'){
+  
+  }  
+// ::>>
+// ::>>
+// ::>>
+if(document.body.section.classList === 'assorted-gemz'){
+  
+  } 
+  if(document.body.section.classList === 'mixed-gemz'){
+  
+  }  
+  if(document.body.section.classList === 'no-gemz'){
+  
+  }
+ 
+  
+  
+}());
+
+// ::>> Notes:
+// ::>> Notes:
+// ::>> Notes:
+
+
